ab02a863215390afe8dfdfdfa994011b02d21e6a
Marcos Valdecy Macedo Costa Leite <mvaldecy11@gmail.com>
2024-06-18 19:15:17 -0300

    feat: curtose e obliquidade

--- Inteligência-Artificial-Uma-Abordagem-de-Aprendizado-de-Máquina/src/main/java/capitulo2/Distribuicao.java
+++ Inteligência-Artificial-Uma-Abordagem-de-Aprendizado-de-Máquina/src/main/java/capitulo2/Distribuicao.java
@@ -3,21 +3,36 @@ package capitulo2;
 import java.util.ArrayList;
 import java.util.Collections;
 
-import dataset.Employee;
+
 import dataset.Service;
 
 public class Distribuicao {
-    
 
-    public static Double obliquidade() {
-        ArrayList<Double> salaries = Service.getSalaries();
+    private static Double momento(ArrayList<Double> data, int k) {
+        Double media = Media.media(data);
+        Double dp = Espalhamento.desvioPadrao(data);
+        int size = data.size();
+        Double soma = 0.;
+        for(int i = 0; i < size; i++) {
+            soma += Math.pow(data.get(i) - media, k);
+        }
+        return soma /((size - 1) * Math.pow(dp, k));
     }
+
+    public static Double obliquidade(ArrayList<Double> data) {
+        return momento(data, 3);
+    }
+
+    public static Double curtose(ArrayList<Double> data) {
+        return momento(data, 4);
+    }
+
+
     public static void main(String[] args) {
         ArrayList<Double> salaries = Service.getSalaries();
-        Double max = Collections.max(salaries).doubleValue();
-        Double min = Collections.min(salaries);
-        System.out.println(salaries.size()); 
-        System.out.println(max);
-        System.out.println(min);
+        Double obliquidade = obliquidade(salaries);
+        Double curtose = curtose(salaries);
+        System.out.println("Obliquidade: " + obliquidade);
+        System.out.println("Curtose: " + curtose);
     }
 }
